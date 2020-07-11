13b58f3f90ff1db6d50baeb45c552f05312a79a1
Noel Escobedo <noel@michelada.io>
2020-07-11 22:38:02 +0000

    Defined components folder

---
+++ components/About/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+// import styles from './styles';
+
+const About = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Acerca de mi app</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default About;
---
+++ components/About/styles.js
@@ -0,0 +1,6 @@
+import {StyleSheet} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const styles = StyleSheet.create({});
+
+export default styles;
